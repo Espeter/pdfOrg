cc261015a89a10f306a4ff36b8385bf19d3e2f39
Espeter <Carl.espeter@wb.de>
2021-01-11 15:47:11 +0100

    init

---
+++ pdfOrg/pdfOrgApp.swift
@@ -0,0 +1,20 @@
+//
+//  pdfOrgApp.swift
+//  pdfOrg
+//
+//  Created by Carl Espeter on 11.01.21.
+//
+
+import SwiftUI
+
+@main
+struct pdfOrgApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
