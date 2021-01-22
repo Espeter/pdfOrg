ea3fe470f964b7642e89cd4f8ba0e7d54aaa3378
Espeter <Carl.espeter@wb.de>
2021-01-22 20:46:54 +0100

    ups vergesse

---
+++ pdfOrg/ui/gig/GigInfoView.swift
@@ -0,0 +1,108 @@
+//
+//  GigInfoView.swift
+//  pdfOrg
+//
+//  Created by Carl Espeter on 21.01.21.
+//
+
+import SwiftUI
+
+struct GigInfoView: View {
+    @Environment(\.managedObjectContext) var viewContext
+
+    @Binding var gig: Gig
+    @Binding var updateView: Bool
+    
+    var body: some View {
+        
+        List{
+            ForEach(getArraySong(gig.songsInGig!)) { songinGig in
+                HStack{
+                Text("\(songinGig.position + 1)")
+                Text("\(songinGig.song!.title!)")
+                }
+            }.onDelete(perform: deleteSong)
+            .onMove(perform: move)
+            
+        }.padding().background(Color(UIColor.white)).cornerRadius(15.0).shadow( radius: 15, x: 3, y: 5)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        
+      //  let source = source.first
+        moveSongInGig(from: source.first!, to: destination)
+        
+    }
+    
+    func getArraySongInGig(_ snSet : NSSet) -> [SongInGig] {
+        var songsInGig = snSet.allObjects as! [SongInGig]
+        
+        songsInGig.sort {
+            //    $0.position > $1.position
+            $0.position < $1.position
+        }
+        
+        return songsInGig
+    }
+    
+    func moveSongInGig(from source: Int, to destination: Int) {
+        
+        var songsInGig = getArraySongInGig(gig.songsInGig!)
+       
+        
+        print("source: \(source) destination: \(destination)")
+        
+        var destinationNew = destination
+        
+        let element = songsInGig.remove(at: source)
+        if destinationNew >= songsInGig.count {
+            destinationNew = destination - 1
+        }
+        songsInGig.insert(element, at: destinationNew )
+        
+        renewPosition(songsInGig: songsInGig)
+        
+    }
+    
+    func renewPosition(songsInGig: [SongInGig]) {
+        
+        var i = 0
+        
+        songsInGig.forEach{ songInGig in
+            
+            songInGig.position = Int64(i)
+            i = i + 1
+        }
+        saveContext()
+        updateView.toggle()
+    }
+    
+    private func deleteSong(offsets: IndexSet) {
+        withAnimation {
+            offsets.map {getArraySong(gig.songsInGig!)[$0]}.forEach(viewContext.delete)
+            saveContext()
+        }
+    }
+    
+    func getArraySong(_ snSet : NSSet) -> [SongInGig] {
+        
+        let songsInGig = snSet.allObjects as! [SongInGig]
+        
+        let sortedSongsInGig = songsInGig.sorted {
+            $0.position < $1.position
+        }
+        
+        return sortedSongsInGig
+    }
+    
+    private func saveContext() {
+        do{
+            try viewContext.save()
+        }
+        catch {
+            let error = error as NSError
+            fatalError("error addBook: \(error)")
+        }
+    }
+    
+}
